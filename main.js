939c8a59f64626a57cb7ba7c7abaa1b3ce123a1e
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2021-12-23 17:42:46 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL("BirthdayImage.jpg", function(img){
+        var image=img;
+        image.scaleToWidth(700);
+        image.scaleToHeight(510);
+        image.set({
+            top:0,
+            left:0
+        });
+        canvas.add(image);
+    });
+}
+
+function playSound(){
+	x.play();
+}
